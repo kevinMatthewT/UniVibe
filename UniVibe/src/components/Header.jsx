bb30288a0534aa0da358c1b393d91a632b7e66db
kevin matthew T <kmturbobro02@gmail.com>
2023-12-04 22:25:59 +0700

    adding more to navbar

--- UniVibe/src/components/Header.jsx
+++ UniVibe/src/components/Header.jsx
@@ -4,15 +4,7 @@ import NavBar  from "./NavBar";
 
 function Header(){
     return(
-        <div className="HeaderContainer">
-            <header>
-
-                <img src={logo} className="image"/>
-
-                <div >User</div>
-                
-            </header>
-        </div>
+        <NavBar/>
     );
 }
 
--- UniVibe/src/components/NavBar.jsx
+++ UniVibe/src/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import logo from "../assets/images/Univibe.png"
 import "../styles/NavBarStyles.css"
+import icon from "../assets/images/UserIcon.png"
 
 function NavBar(){
     return(
-        <div className="HeaderContainer">
-            <header>
-
-                <img src={logo} className="image"/>
-
-                <div >User</div>
+        <div className="NavBarContainer">
+            <img src={logo} className="image"/>
+                <ul>
+                <li></li>
                 
-            </header>
+                <li><div className="navbarElements"><a href=""><img src={icon}/></a></div></li>
+                <li><div className="navbarElements">hello</div></li>
+                </ul>
         </div>
     );
 }
